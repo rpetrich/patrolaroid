a78984f0c6a352099a5591f71cd796d881229c8a
Ryan Petrich <rpetrich@gmail.com>
2021-05-28 13:56:24 -0400

    Allow scanning multiple volume IDs and default to scanning all volumes in the AZ

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -18,58 +19,10 @@ import (
 	yara "github.com/capsule8/go-yara"
 )
 
-func main() {
-	ctx := context.Background()
-	// load YARA
-	compiler, err := yara.NewCompiler()
-	if err != nil {
-		log.Fatalf("Could not initialize yara compiler: %v", err)
-	}
-	defer compiler.Destroy()
-	// read rules from filesystem
-	ruleCount := 0
-	err = filepath.Walk("./signatures", func(recursivePath string, info os.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-		if info.IsDir() {
-			return nil
-		}
-		ext := filepath.Ext(recursivePath)
-		if ext != ".yar" && ext != ".yara" {
-			return nil
-		}
-		log.Printf("loading yara rule from %q", recursivePath)
-		f, err := os.Open(recursivePath)
-		if err != nil {
-			return fmt.Errorf("error opening yara signature from %q: %v", recursivePath, err)
-		}
-		defer f.Close()
-		err = compiler.AddFile(f, recursivePath)
-		if err != nil {
-			log.Printf("error compiling yara signature from %q: %v", recursivePath, err)
-		} else {
-			ruleCount++
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatalf("could not initialize yara rules: %v", err)
-	}
-	// compile the rules
-	r, err := compiler.GetRules()
-	if err != nil {
-		log.Fatalf("failed to compile yara rules: %s", err)
-		return
-	}
-	defer r.Destroy()
-	if ruleCount == 0 {
-		log.Fatalf("no rules to scan files with; place signatures in ./signatures/*.yar")
-	}
+func run() int {
 	// load the AWS config
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion("us-east-1"),
-	)
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatalf("unable to load AWS config: %v", err)
 	}
@@ -103,86 +56,153 @@ func main() {
 		log.Fatalf("failed to read instance id content: %v", err)
 	}
 	instanceId := string(instanceIdBytes)
-	// connect to EC2
-	if len(os.Args) < 2 {
-		log.Printf("must specify a volume to snapshot as command line argument")
-		return
+	// parse arguments
+	signaturePathFlag := flag.String("signatures", "./signatures", "a path to YARA signatures")
+	volumeIdsFlag := flag.String("volume-ids", "", "a comma separated list of volume IDs to scan")
+	flag.Parse()
+	// load YARA
+	compiler, err := yara.NewCompiler()
+	if err != nil {
+		log.Fatalf("Could not initialize yara compiler: %v", err)
 	}
-	volumeId := os.Args[1]
-	dryRun := false
-	// create a snapshot
-	var snapshotId string
-	client := ec2.NewFromConfig(cfg)
-	if len(os.Args) >= 3 {
-		snapshotId = os.Args[2]
-		log.Printf("using existing snapshot %q", snapshotId)
-	} else {
-		log.Printf("creating new snapshot for %q", volumeId)
-		description := "crassidens temporary snapshot of " + volumeId
-		snapshot, err := client.CreateSnapshot(ctx, &ec2.CreateSnapshotInput{
-			VolumeId:    &volumeId,
-			Description: &description,
-			DryRun:      &dryRun,
-		})
+	defer compiler.Destroy()
+	// read rules from filesystem
+	ruleCount := 0
+	err = filepath.Walk(*signaturePathFlag, func(recursivePath string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Fatalf("create snapshot request failed: %v", err)
+			return nil
 		}
-		snapshotId = *snapshot.SnapshotId
-		log.Printf("created snapshot: %s", snapshotId)
+		if info.IsDir() {
+			return nil
+		}
+		ext := filepath.Ext(recursivePath)
+		if ext != ".yar" && ext != ".yara" {
+			return nil
+		}
+		log.Printf("loading yara rule from %q", recursivePath)
+		f, err := os.Open(recursivePath)
+		if err != nil {
+			return fmt.Errorf("error opening yara signature from %q: %v", recursivePath, err)
+		}
+		defer f.Close()
+		err = compiler.AddFile(f, recursivePath)
+		if err != nil {
+			log.Printf("error compiling yara signature from %q: %v", recursivePath, err)
+		} else {
+			ruleCount++
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("could not initialize yara rules: %v", err)
 	}
-	// create a volume for the snapshot
-	var snapshotVolumeId string
-	if len(os.Args) >= 4 {
-		snapshotVolumeId = os.Args[3]
-		log.Printf("using existing snapshot volume %q", volumeId)
+	// compile the rules
+	r, err := compiler.GetRules()
+	if err != nil {
+		log.Fatalf("failed to compile yara rules: %s", err)
+	}
+	defer r.Destroy()
+	if ruleCount == 0 {
+		log.Fatalf("no rules to scan files with; place signatures in ./signatures/*.yar")
+	}
+	// connect to EC2
+	client := ec2.NewFromConfig(cfg)
+	dryRun := false
+	// search for volumes
+	var volumeIds []string
+	if *volumeIdsFlag != "" {
+		volumeIds = strings.Split(*volumeIdsFlag, ",")
 	} else {
-		snapshotIdKey := "snapshot-id"
-		fiveSnapshots := int32(5)
-	wait_for_snapshot_completion:
+		var nextToken *string
 		for {
-			log.Printf("describing snapshot %q", snapshotId)
-			descriptions, err := client.DescribeSnapshots(ctx, &ec2.DescribeSnapshotsInput{
-				DryRun: &dryRun,
-				Filters: []types.Filter{
-					types.Filter{
-						Name:   &snapshotIdKey,
-						Values: []string{snapshotId},
-					},
-				},
-				MaxResults: &fiveSnapshots,
+			volumes, err := client.DescribeVolumes(ctx, &ec2.DescribeVolumesInput{
+				DryRun:    &dryRun,
+				NextToken: nextToken,
 			})
 			if err != nil {
-				log.Fatalf("unable to query snapshots: %v", err)
+				log.Fatalf("describe volumes request failed: %v", err)
 			}
-			for _, snapshot := range descriptions.Snapshots {
-				switch snapshot.State {
-				case types.SnapshotStateError:
-					if snapshot.StateMessage != nil {
-						log.Fatalf("creating snapshot failed: %s", *snapshot.StateMessage)
-					} else {
-						log.Fatalf("creating snapshot failed without message")
-					}
-				case types.SnapshotStateCompleted:
-					log.Printf("snapshot completed")
-					break wait_for_snapshot_completion
-				case types.SnapshotStatePending:
-					if snapshot.Progress != nil {
-						log.Printf("snapshot progress: %s", *snapshot.Progress)
-					}
-				}
+			for _, volume := range volumes.Volumes {
+				volumeIds = append(volumeIds, *volume.VolumeId)
+				log.Printf("found volume %s", *volume.VolumeId)
 			}
-			time.Sleep(10 * time.Second)
+			if nextToken = volumes.NextToken; nextToken == nil {
+				break
+			}
 		}
-		createVolumeResponse, err := client.CreateVolume(ctx, &ec2.CreateVolumeInput{
-			AvailabilityZone: &az,
-			SnapshotId:       &snapshotId,
+	}
+	exitCode := 0
+	for _, volumeId := range volumeIds {
+		if err = processVolume(ctx, client, az, instanceId, volumeId, r); err != nil {
+			log.Printf("%v", err)
+			exitCode = 1
+		}
+	}
+	return exitCode
+}
+
+func processVolume(ctx context.Context, client *ec2.Client, az string, instanceId string, volumeId string, rules *yara.Rules) error {
+	// create a snapshot
+	dryRun := false
+	log.Printf("creating new snapshot for %q", volumeId)
+	description := "Patrolaroid temporary snapshot of " + volumeId
+	snapshot, err := client.CreateSnapshot(ctx, &ec2.CreateSnapshotInput{
+		VolumeId:    &volumeId,
+		Description: &description,
+		DryRun:      &dryRun,
+	})
+	if err != nil {
+		return fmt.Errorf("create snapshot request failed: %v", err)
+	}
+	snapshotId := *snapshot.SnapshotId
+	log.Printf("created snapshot: %s", snapshotId)
+	// create a volume for the snapshot
+	snapshotIdKey := "snapshot-id"
+	fiveSnapshots := int32(5)
+wait_for_snapshot_completion:
+	for {
+		log.Printf("describing snapshot %q", snapshotId)
+		descriptions, err := client.DescribeSnapshots(ctx, &ec2.DescribeSnapshotsInput{
+			DryRun: &dryRun,
+			Filters: []types.Filter{
+				types.Filter{
+					Name:   &snapshotIdKey,
+					Values: []string{snapshotId},
+				},
+			},
+			MaxResults: &fiveSnapshots,
 		})
 		if err != nil {
-			log.Fatalf("create volume request failed: %v", err)
+			return fmt.Errorf("unable to query snapshots: %v", err)
 		}
-		snapshotVolumeId = *createVolumeResponse.VolumeId
-		log.Printf("created snapshot volume: %s", snapshotVolumeId)
+		for _, snapshot := range descriptions.Snapshots {
+			switch snapshot.State {
+			case types.SnapshotStateError:
+				if snapshot.StateMessage != nil {
+					return fmt.Errorf("creating snapshot failed: %s", *snapshot.StateMessage)
+				} else {
+					return fmt.Errorf("creating snapshot failed without message")
+				}
+			case types.SnapshotStateCompleted:
+				log.Printf("snapshot completed")
+				break wait_for_snapshot_completion
+			case types.SnapshotStatePending:
+				if snapshot.Progress != nil {
+					return fmt.Errorf("snapshot progress: %s", *snapshot.Progress)
+				}
+			}
+		}
+		time.Sleep(10 * time.Second)
 	}
+	createVolumeResponse, err := client.CreateVolume(ctx, &ec2.CreateVolumeInput{
+		AvailabilityZone: &az,
+		SnapshotId:       &snapshotId,
+	})
+	if err != nil {
+		return fmt.Errorf("create volume request failed: %v", err)
+	}
+	snapshotVolumeId := *createVolumeResponse.VolumeId
+	log.Printf("created snapshot volume: %s", snapshotVolumeId)
 	// wait for the volume to become ready
 	var attached bool
 wait_for_volume_completion:
@@ -201,10 +221,10 @@ wait_for_volume_completion:
 			MaxResults: &fiveVolumes,
 		})
 		if err != nil {
-			log.Fatalf("unable to query volumes: %v", err)
+			return fmt.Errorf("unable to query volumes: %v", err)
 		}
 		if len(descriptions.Volumes) == 0 {
-			log.Fatalf("invalid volume id")
+			return fmt.Errorf("invalid volume id: %q", volumeId)
 		}
 		for _, volume := range descriptions.Volumes {
 			switch volume.State {
@@ -219,11 +239,11 @@ wait_for_volume_completion:
 				attached = true
 				break wait_for_volume_completion
 			case types.VolumeStateDeleting:
-				log.Fatalf("volume is being deleted")
+				return fmt.Errorf("volume is being deleted")
 			case types.VolumeStateDeleted:
-				log.Fatalf("volume is deleted")
+				return fmt.Errorf("volume is deleted")
 			case types.VolumeStateError:
-				log.Fatalf("volume is in error state")
+				return fmt.Errorf("volume is in error state")
 			}
 		}
 		time.Sleep(10 * time.Second)
@@ -237,7 +257,7 @@ wait_for_volume_completion:
 			Device:     &device,
 		})
 		if err != nil {
-			log.Fatalf("attach volume request failed: %v", err)
+			return fmt.Errorf("attach volume request failed: %v", err)
 		}
 	wait_for_volume_attachment:
 		for {
@@ -255,7 +275,7 @@ wait_for_volume_completion:
 				MaxResults: &fiveVolumes,
 			})
 			if err != nil {
-				log.Fatalf("unable to query volumes: %v", err)
+				return fmt.Errorf("unable to query volumes: %v", err)
 			}
 			for _, volume := range descriptions.Volumes {
 				for _, attachment := range volume.Attachments {
@@ -269,20 +289,20 @@ wait_for_volume_completion:
 						case types.VolumeAttachmentStateDetaching:
 							log.Printf("volume is detaching")
 						case types.VolumeAttachmentStateDetached:
-							log.Fatalf("volume is detached")
+							return fmt.Errorf("volume is detached, but expected it to be in another state")
 						case types.VolumeAttachmentStateBusy:
-							log.Fatalf("volume is busy")
+							return fmt.Errorf("volume is busy, but expected it to be in another state")
 						}
 					}
 				}
 			}
-			time.Sleep(10 * time.Second)
+			time.Sleep(5 * time.Second)
 		}
 	}
 	// find the newest nvme devices
 	files, err := ioutil.ReadDir("/dev")
 	if err != nil {
-		log.Fatalf("unable to read local devices: %v", err)
+		return fmt.Errorf("unable to read local devices: %v", err)
 	}
 	var newestDevice string
 	for _, file := range files {
@@ -291,14 +311,14 @@ wait_for_volume_completion:
 		}
 	}
 	if newestDevice == "" {
-		log.Fatalf("unable to find any nvme devices")
+		return fmt.Errorf("unable to find any nvme devices")
 	}
 	log.Printf("latest device is %s", newestDevice)
 	// mount the device
 	_ = os.Mkdir("./snapshot", 0644)
 	err = syscall.Mount(newestDevice, "./snapshot", "ext4", syscall.MS_RDONLY|syscall.MS_NOEXEC, "")
 	if err != nil {
-		log.Fatalf("failed to mount device: %v", err)
+		return fmt.Errorf("failed to mount device: %v", err)
 	}
 	log.Printf("mounted successfully")
 	// scan the mount
@@ -315,7 +335,7 @@ wait_for_volume_completion:
 		}
 		// Actually scan the file
 		var m yara.MatchRules
-		err = r.ScanFile(recursivePath, 0, 0, &m)
+		err = rules.ScanFile(recursivePath, 0, 0, &m)
 		if err != nil {
 			errorText := err.Error()
 			// Ignore bad symlinks and unlink race conditions. Have to
@@ -328,7 +348,7 @@ wait_for_volume_completion:
 		}
 		// If we have matches, dispatch an alert
 		if len(m) != 0 {
-			log.Printf("file at path %q violated rule %q from %q", strings.TrimPrefix(recursivePath, "snapshot"), m[0].Rule, m[0].Namespace)
+			log.Printf("file in volume %s at path %q violated rule %q from %q", volumeId, strings.TrimPrefix(recursivePath, "snapshot"), m[0].Rule, m[0].Namespace)
 		}
 		return nil
 	})
@@ -346,7 +366,7 @@ wait_for_volume_completion:
 		VolumeId:   &snapshotVolumeId,
 	})
 	if err != nil {
-		log.Fatalf("detach volume request failed: %v", err)
+		return fmt.Errorf("detach volume request failed: %v", err)
 	}
 wait_for_volume_detachment:
 	for {
@@ -364,7 +384,7 @@ wait_for_volume_detachment:
 			MaxResults: &fiveVolumes,
 		})
 		if err != nil {
-			log.Fatalf("unable to query volumes: %v", err)
+			return fmt.Errorf("unable to query volumes: %v", err)
 		}
 		for _, volume := range descriptions.Volumes {
 			switch volume.State {
@@ -376,13 +396,11 @@ wait_for_volume_detachment:
 			case types.VolumeStateInUse:
 				log.Printf("volume is in use, waiting for detachment")
 			case types.VolumeStateDeleting:
-				log.Fatalf("volume is being deleted")
-				break wait_for_volume_detachment
+				log.Printf("volume is being deleted")
 			case types.VolumeStateDeleted:
-				log.Fatalf("volume is deleted")
-				break wait_for_volume_detachment
+				return nil
 			case types.VolumeStateError:
-				log.Fatalf("volume is in error state")
+				return fmt.Errorf("volume is in error state")
 			}
 		}
 		time.Sleep(10 * time.Second)
@@ -393,6 +411,11 @@ wait_for_volume_detachment:
 		VolumeId: &snapshotVolumeId,
 	})
 	if err != nil {
-		log.Fatalf("detach volume request failed: %v", err)
+		return fmt.Errorf("delete volume request failed: %v", err)
 	}
+	return nil
+}
+
+func main() {
+	os.Exit(run())
 }
